4bdfba1600e56ce79a216ff0d9aa95578397640c
pb-korolev <pavel.b.korolev@my.games>
2024-08-18 20:08:58 +0300

    reset script

--- picker/__init__.py
+++ picker/__init__.py
@@ -1,4 +1,12 @@
-import sys 
+import sys, os
+import maya.mel as mel
+
+# load mel script
+root_path = os.path.dirname(os.path.abspath(__file__))[:-6] + "animTools"
+reset_script_file = os.path.join(root_path, "resetToDefaultValues.mel")
+cmd = 'source "%s"' %(reset_script_file.replace("\\", "/"))
+mel.eval(cmd)
+
 
 if sys.version[0] == "2":
 	import main, data_handler
